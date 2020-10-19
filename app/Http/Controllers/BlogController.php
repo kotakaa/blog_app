999b55031d29e4c2a39349690fc100765166879e
小高優香 <kotaka.yuuka.infratop@gmail.com>
2020-10-20 00:46:19 +0900

    [Add]edit/delete

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
 
 class BlogController extends Controller
 {
@@ -12,6 +14,116 @@ class BlogController extends Controller
      */
     public function showList()
     {
-        return view('blog.list');
+        // dbからデータを全て受け取る
+        $blogs = Blog::all();
+        return view('blog.list', ['blogs' => $blogs]);
+    }
+
+    /**
+     * ブログ詳細を表示する
+     * @param int $id
+     * @return view
+     */
+    // routeから渡ってきたidを引数にしている
+    public function showDetail($id)
+    {
+        $blog = Blog::find($id);
+        if(is_null($blog)) {
+            \Session::flash('err_msg', 'データがありません');
+            return redirect(route('blogs'));
+        }
+        return view('blog.detail', ['blog' => $blog]);
+    }
+
+    /**
+     * ブログ登録画面を表示する
+     * @return view
+     */
+    public function showCreate()
+    {
+        return view('blog.form');
+    }
+
+    /**
+     * ブログ登録する
+     * @return view
+     */
+    public function exeStore(BlogRequest $request)
+    {
+        $input = $request->all();
+        \DB::beginTransaction();
+        try{
+            Blog::create($input);
+            \DB::commit();
+        }catch(\Throwable $e){
+            \DB::rollback();
+            abort(500);
+        }
+        \Session::flash('err_msg', 'ブログを登録しました。');
+        return redirect(route('blogs'));
+    }
+
+    /**
+     * ブログの編集画面を表示する
+     * @param int $id
+     * @return view
+     */
+    // routeから渡ってきたidを引数にしている
+    public function showEdit($id)
+    {
+        $blog = Blog::find($id);
+        if(is_null($blog)) {
+            \Session::flash('err_msg', 'データがありません');
+            return redirect(route('blogs'));
+        }
+        return view('blog.edit', ['blog' => $blog]);
+    }
+
+    /**
+     * ブログ更新する
+     * @return view
+     */
+    public function exeUpdate(BlogRequest $request)
+    {
+        $input = $request->all();
+        \DB::beginTransaction();
+        try{
+            $blog = Blog::find($input['id']);
+            $blog->fill([
+                'title' => $input['title'],
+                'content' => $input['content']
+            ]);
+            $blog->save();
+            \DB::commit();
+        }catch(\Throwable $e){
+            \DB::rollback();
+            abort(500);
+        }
+        \Session::flash('err_msg', 'ブログを更新しました。');
+        return redirect(route('blogs'));
+    }
+
+
+    /**
+     * ブログの編集画面を表示する
+     * @param int $id
+     * @return view
+     */
+    // routeから渡ってきたidを引数にしている
+    public function exeDelete($id)
+    {
+        if(empty($id)) {
+            \Session::flash('err_msg', 'データがありません');
+            return redirect(route('blogs'));
+        }
+
+        try{
+            Blog::destroy($id);
+        }catch(\Throwable $e){
+            abort(500);
+        }
+        $blog = Blog::destroy($id);
+        \Session::flash('err_msg', '削除しました。');
+        return redirect(route('blogs'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,21 @@ use Illuminate\Support\Facades\Route;
 
 // ブログの一覧画面表示
 // Route::resource('/', [BlogController::class, 'showList']);
-Route::get('/','App\Http\Controllers\BlogController@showList');
+Route::get('/','App\Http\Controllers\BlogController@showList')->name('blogs');
+
+// ブログの登録画面表示
+Route::get('/blog/create','App\Http\Controllers\BlogController@showCreate')->name('create');
+// ブログの登録表示
+Route::post('/blog/store','App\Http\Controllers\BlogController@exeStore')->name('store');
+
+// ブログの詳細画面表示
+Route::get('/blog/{id}','App\Http\Controllers\BlogController@showDetail')->name('show');
+
+// ブログの編集画面表示
+Route::get('/blog/edit/{id}','App\Http\Controllers\BlogController@showEdit')->name('edit');
+
+// ブログの編集表示
+Route::post('/blog/update','App\Http\Controllers\BlogController@exeUpdate')->name('update');
+
+// ブログの削除
+Route::post('/blog/delete/{id}','App\Http\Controllers\BlogController@exeDelete')->name('delete');
